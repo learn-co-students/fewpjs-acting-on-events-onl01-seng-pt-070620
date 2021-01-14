bc1e121efcd511b268a192f5551d6e20bb59ceeb
Zainab-Omar <zainab.alshaikhli01@gmail.com>
2021-01-13 21:44:23 -0600

    Done.

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ function moveDodgerLeft() {
 function moveDodgerRight() {
     let rightNumbers = dodger.style.left.replace("px", "");
     let right = parseInt(rightNumbers, 10);
-    if (right > 0) { 
-        dodger.style.left = `${right + 1}px`
+    if (right < 360) { 
+        console.log(dodger.style.left = `${right + 1}px`);
+
     }
 
 }
