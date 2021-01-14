e7f6ad2d8c578d6d17568b57a93cbe0a0e508328
JakeKViti <thevitij64@gmail.com>
2021-01-14 00:54:46 -0500

    Done.

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ function moveDodgerLeft() {
   }
 
   function moveDodgerRight() {
-    var leftNumbers = dodger.style.left.replace('px', '');
-    var left = parseInt(leftNumbers, 10)
-    if (left > 0) {
-      dodger.style.left = `${left + 1}px`
+    var rightNumbers = dodger.style.right.replace('px', '');
+    var right = parseInt(rightNumbers, 10)
+    if (right > 0) {
+      dodger.style.right = `${right + 1}px`
     }
   }
   
